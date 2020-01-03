30db47b83055e600c21a2c52ccc1326cff7fdbea
ifer-itcast <yangkangkang@itcast.cn>
2020-01-03 15:12:54 +0800

    登录之前端校验

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -1,8 +1,8 @@
 function arrToJSON(form) {
     let arr = form.serializeArray();
-    let result = {};
-    arr.forEach(item => {
-        result[item.name] = item.value;
+    let obj = {};
+    arr.forEach((item, index) => {
+        obj[item.name] = item.value;
     });
-    return result;
+    return obj;
 }
