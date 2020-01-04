d25ad626ee8dcb0c9267ce468cb5f4b1f17dbce0
ifer-itcast <yangkangkang@itcast.cn>
2020-01-04 14:14:21 +0800

    校验用户信息是否符合规则

--- route/admin/user-edit-fn.js
+++ route/admin/user-edit-fn.js
@@ -1,3 +1,21 @@
-module.exports = (req, res) => {
-    res.send(req.body);
+const Joi = require('joi');
+module.exports = async (req, res) => {
+    // 定义规则
+    const schema = {
+        username: Joi.string().min(2).max(12).required().error(new Error('用户名不符合规则')),
+        email: Joi.string().email().error(new Error('邮箱格式不符合要求')),
+        password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required().error(new Error('密码格式不符合要求')),
+        role: Joi.string().valid('normal', 'admin').required().error(new Error('角色非法')),
+        state: Joi.number().valid(0, 1).required().error(new Error('状态值非法'))
+    };
+
+    try {
+        await Joi.validate(req.body, schema);
+    } catch (err) {
+        // err.message
+        return res.redirect(`/admin/user-edit?message=${err.message}`);
+    }
+    
+    // 说明前面的校验规则都通过了
+    
 };
