f544d1035b2996e01d92c7ea87cd60448cb102f8
ifer-itcast <yangkangkang@itcast.cn>
2020-01-04 09:57:13 +0800

    .

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.set('view engine', 'art');
 app.use('/admin', (req, res, next) => {
     // 如果用户访问的不是 /login，并且如果用户的 session 信息不存在
     if (req.url !== '/login' && !req.session.username) {
-        // 让登录
+        // 让重新登录
         res.redirect('/admin/login');
     } else {
         next();
