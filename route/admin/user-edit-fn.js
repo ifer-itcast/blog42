7e932523a6590a51a7a63fdaf77fdad149810e89
ifer-itcast <yangkangkang@itcast.cn>
2020-01-04 14:52:53 +0800

    添加用户

--- route/admin/user-edit-fn.js
+++ route/admin/user-edit-fn.js
@@ -1,4 +1,7 @@
 const Joi = require('joi');
+const { User } = require('../../model/user');
+const hash = require('../../utils/hash');
+
 module.exports = async (req, res) => {
     // 定义规则
     const schema = {
@@ -17,5 +20,19 @@ module.exports = async (req, res) => {
     }
     
     // 说明前面的校验规则都通过了
-    
+    // 先检测邮箱是否已存在
+    let user = await User.findOne({email: req.body.email});
+    if (user) {
+        // 说明此邮箱已存在，不允许重复添加
+        return res.redirect(`/admin/user-edit?message=此邮箱已存在`);
+    }
+
+    // 对传递过来的密码进行加密
+    req.body.password = hash(req.body.password);
+
+    // 添加到数据
+    await User.create(req.body);
+
+    // 跳转到用户界面
+    res.redirect('/admin/user');
 };
