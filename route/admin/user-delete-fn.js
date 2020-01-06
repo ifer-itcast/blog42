54e2a0e8cec0db2f9a62154b59bbb5e49682c498
ifer-itcast <yangkangkang@itcast.cn>
2020-01-06 14:40:54 +0800

    删除用户

--- route/admin/user-delete-fn.js
+++ route/admin/user-delete-fn.js
@@ -3,11 +3,12 @@ module.exports = async (req, res) => {
     try {
         await User.findOneAndDelete({_id: req.query.id});
     } catch (err) {
+        // 传递的 ID 不存在，会删除失败，那就跳转到用户列表页面
         return next(JSON.stringify({
             path: '/admin/user',
             message: '删除失败'
         }));
     }
-    
+    // 删除成功也跳转到用户列表
     res.redirect('/admin/user');
 };
