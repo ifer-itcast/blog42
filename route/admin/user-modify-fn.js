0e209304a366c6f1fe298220e3c68674789392bd
ifer-itcast <yangkangkang@itcast.cn>
2020-01-06 10:49:50 +0800

    用户信息的真正修改操作

--- route/admin/user-modify-fn.js
+++ route/admin/user-modify-fn.js
@@ -4,14 +4,21 @@ const hash = require('../../utils/hash');
 module.exports = async (req, res, next) => {
     // 修改前的密码比对
     // 传递过来的密码和查询出来的密码进行比较
-    const {password} = req.body;
+    const {password, username, email, state, role} = req.body;
     const id = req.query.id;
 
     let user = await User.findOne({_id: id});
 
     if(hash(password) === user.password) {
         // 允许修改
-        res.send('ok');
+        await User.updateOne({_id: id}, {
+            username,
+            email,
+            role,
+            state
+        });
+        // 修改成功后跳转到用户列表
+        res.redirect('/admin/user');
     } else {
         // 不允许修改
         let obj = {
