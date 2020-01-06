7e0b0c7080fd5439b204ce394fb3109db5b22974
ifer-itcast <yangkangkang@itcast.cn>
2020-01-06 09:33:09 +0800

    修改用户前展示原来的信息

--- route/admin/user-edit.js
+++ route/admin/user-edit.js
@@ -1,6 +1,24 @@
-module.exports = (req, res) => {
-    const { message } = req.query;
-    res.render('admin/user-edit', {
-        message
-    });
+const { User } = require('../../model/user');
+
+module.exports = async (req, res) => {
+    const { message, id } = req.query;
+
+    // 根据是否有 id 来区分是修改还是添加
+    if (id) {
+        let user = await User.findOne({_id: id});
+        // 修改
+        res.render('admin/user-edit', {
+            message,
+            user,
+            link: '/admin/user-modify',
+            buttonText: '修改'
+        });
+    } else {
+        // 添加
+        res.render('admin/user-edit', {
+            message,
+            link: '/admin/user-edit',
+            buttonText: '添加'
+        });
+    }
 };
