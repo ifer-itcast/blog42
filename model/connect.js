f183a7e6dabf92e2878997f288ff054c590ec5b3
ifer-itcast <yangkangkang@itcast.cn>
2020-01-07 15:38:09 +0800

    使用config模块

--- model/connect.js
+++ model/connect.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
+const config = require('config');
 
+// mongodb://elser:elser@localhost/blog42
 mongoose
-	.connect('mongodb://elser:elser@localhost/blog42', { useNewUrlParser: true, useUnifiedTopology: true })
+	.connect(
+		`mongodb://${config.get('db.user')}:${config.get('db.pwd')}@${config.get('db.host')}:${config.get('db.port')}/${config.get('db.name')}`,
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	)
 	.then(() => console.log('~~~~~~~~数据连接成功~~~~~~~~'))
 	.catch(err => console.log(err, '数据连接失败'));
