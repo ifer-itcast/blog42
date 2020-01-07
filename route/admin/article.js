d92d1c361f25ab71211c8ccf82d50cdb3fe090a3
ifer-itcast <yangkangkang@itcast.cn>
2020-01-07 10:52:42 +0800

    文章列表分页

--- route/admin/article.js
+++ route/admin/article.js
@@ -1,11 +1,16 @@
+const pagination = require('mongoose-sex-page');
 const { Article } = require('../../model/article');
 
 module.exports = async (req, res) => {
     // 添加 article 标识，证明是文章相关的页面
     req.app.locals.currentLink = 'article';
-
-    let articles = await Article.find().populate('author')
-
+    let page = req.query.page || 1;
+    // page => 代表当前页
+    // size => 一页显示多少条
+    // display => 要展示多少页
+    // exec => 执行前面的查询操作
+    let articles = await pagination(Article).find().page(page).size(2).display(5).populate('author').exec();
+    // return res.send(articles);
     res.render('admin/article', {
         username: req.session.username,
         articles
