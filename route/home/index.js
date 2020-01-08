786d5ccb4ed35cd20c1d2d2f17ea9438bff46f66
ifer-itcast <yangkangkang@itcast.cn>
2020-01-08 09:57:11 +0800

    前台显示文章列表

--- route/home/index.js
+++ route/home/index.js
@@ -1,3 +1,15 @@
-module.exports = (req, res) => {
-    res.render('home/default');
+const { Article } = require('../../model/article');
+const pagination = require('mongoose-sex-page');
+
+module.exports = async (req, res) => {
+    let page = req.query.page || 1;
+    // page => 显示哪一页
+    // size => 一页显示多少条
+    // display => 前端需要展示多少页
+    // exec    => 执行前面的查询操作
+    let articles = await pagination(Article).find().populate('author').page(page).size(2).display(5).exec();
+    // res.send(articles);
+    res.render('home/default', {
+        articles
+    });
 };
